554da1a1299a98b76780148dbf6795c7d52c5952
inukkaki <kgn.gkrk.rstb@gmail.com>
2026-02-16 13:45:56 +0900

    Create Texture class

--- src/routine/debug.cpp
+++ src/routine/debug.cpp
@@ -4,6 +4,7 @@
 
 #include "SDL2/SDL.h"
 
+#include "graphics/texture.h"
 #include "interface/keyboard.h"
 #include "interface/mouse.h"
 #include "routine/event.h"
@@ -14,6 +15,7 @@ namespace windfall::routine::debug {
 
 namespace impl {
 
+namespace texture = windfall::graphics::texture;
 namespace kbd = windfall::interface::keyboard;
 namespace mouse = windfall::interface::mouse;
 namespace revent = windfall::routine::event;
@@ -24,6 +26,33 @@ namespace framerate = windfall::time::framerate;
 
 void DebugRoutine(SDL_Window* window, SDL_Renderer* renderer)
 {
+    // Graphics
+    SDL_SetRenderTarget(renderer, nullptr);
+    SDL_SetRenderDrawColor(renderer, 0x1f, 0x3f, 0x6f, 0xff);
+    SDL_RenderClear(renderer);
+
+    impl::texture::Texture texture(renderer);
+    texture.CreateTexture(320, 240);
+    texture.Clear(0x00, 0x00, 0x00, 0x7f);
+
+    texture.SetDrawColor(0xff, 0xff, 0xff, 0xff);
+    texture.DrawRect(0, 0, 320, 240);
+    texture.DrawLine(0, 0, 4, 4);
+    texture.DrawLine(10, 5, 200, 220);
+    texture.DrawPoint(317, 3);
+    texture.SetDrawColor(0xff, 0xff, 0x00, 0x4f);
+    texture.FillRect(20, 120, 120, 80);
+
+    impl::texture::RenderRect src1(0, 0, 320, 240);
+    texture.Render(nullptr, src1, 1.5f, 1.4f);
+    impl::texture::RenderRect src2(0, 0, 60, 80, 10, 5);
+    impl::texture::RenderRect dst1(322, 1, 60, 80);
+    texture.Render(nullptr, src2, dst1);
+    impl::texture::RenderRect dst2(350, 100, 60, 80);
+    texture.Render(nullptr, src1, dst2);
+
+    SDL_RenderPresent(renderer);
+
     // Keyboard & mouse
     impl::kbd::Keyboard kbd;
     impl::mouse::Mouse mouse;
@@ -38,20 +67,6 @@ void DebugRoutine(SDL_Window* window, SDL_Renderer* renderer)
     while (!quits) {
         // Handle events
         quits = impl::revent::HandleEvents(kbd, mouse);
-
-        // DEBUG
-        //std::cout << "x:" << mouse.x() << " y:" << mouse.y()
-        //          << " dx:" << mouse.dx() << " dy:" << mouse.dy()
-        //          << " scrolls:" << mouse.scrolls() << std::endl;
-        if (mouse.Presses(impl::mouse::ButtonCode::kLeft)) {
-            std::cout << "L" << std::endl;
-        }
-        if (mouse.Pressing(impl::mouse::ButtonCode::kRight)) {
-            std::cout << "R" << std::endl;
-        }
-        if (mouse.Releases(impl::mouse::ButtonCode::kMiddle)) {
-            std::cout << "M" << std::endl;
-        }
 
         // Keyboard & mouse
         kbd.Update();
